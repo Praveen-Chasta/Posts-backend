558eb1a0f1756320d78cfe7e46bc6d67466cfea3
Praveen-Chasta <praveen.chastaa@gmail.com>
2025-02-03 12:17:10 +0530

    third commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    origin: ['http://localhost:5174', 'http://localhost:5173', 'https://quick-posts.vercel.app/'],
     credentials: true
 }));
 
